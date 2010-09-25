17932e7ac7ec29be0d943050b5f8d5d7418c330f
Anders Johannsen <anders@johannsen.com>
2010-09-25 15:03:42 +0200

    Autotest working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'active_schema'
 require 'rspec'
-require 'rspec/autorun'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
